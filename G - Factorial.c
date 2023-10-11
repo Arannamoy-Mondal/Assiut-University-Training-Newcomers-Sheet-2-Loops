1e8cb4adb8bbd51b65e12c5d5c516b2a4d99323c
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2023-10-12 00:42:50 +0600

    Create G - Factorial.c

---
+++ G - Factorial.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  long long n,i;
+  scanf("%lld",&n);
+  for (i=1;i<=n; i++)
+  {
+    long long a,e=1;
+    scanf("%lld",&a);
+    while (a!=0)
+  {
+    e=e*a;
+    a=a-1;
+   }
+   printf("%lld\n",e);
+    
+  }
+  
+
+  
+  
+    return 0;
+}
